75033459968d4aeb79dfe6c4908b3bcadbd2f02d
jossM <josselin.mahe@gmail.com>
2018-10-28 21:31:04 +0100

    feat: shutdown after backup is over at 4

--- xbackupscript.sh
+++ xbackupscript.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 source /home/josselin/.bashrc
-s3cmd sync $largedisk/archive/ s3://jmahe --server-side-encryption &>> /home/josselin/backupscriptlogs.txt
+
+set -x
+
+shopt -s lastpipe
+date +'%Y-%m-%d %H:%M' |
+	read date_backup hm
+echo $hm |
+	IFS=: read heure_backup x
+s3cmd sync $largedisk/archive/ s3://jmahe --server-side-encryption &>> /home/josselin/backupscript_logs/$date_backup-$hm.log
+
+who -r |
+	read x x x date_boot hm
+echo $hm |
+	IFS=: read heure_boot x
+
+if [[ $date_boot = $date_backup && $heure_boot = 4 && $heure_backup = 4 ]] ;then
+	echo "Arret systeme" >&2
+	echo shutdown
+else
+	echo "Lancement manuel: pas d'arret systeme"
+fi
