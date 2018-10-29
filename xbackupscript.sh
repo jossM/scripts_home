9799b123652491a7608990fbe14164b53a716640
jossM <josselin.mahe@gmail.com>
2018-10-29 07:30:26 +0100

    fix not working bashrc usage

--- xbackupscript.sh
+++ xbackupscript.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-source ~/.bashrc
 
 shopt -s lastpipe
 date +'%Y-%m-%d %H:%M' |
 	read date_backup hm
 echo $hm |
 	IFS=: read heure_backup x
-s3cmd sync $largedisk/archive/ s3://jmahe --server-side-encryption &>> ~/backupscript_logs/$date_backup-$hm.log
+
+exec &>>~/backupscript_logs/$date_backup-$hm.log 
+
+s3cmd sync /mnt/md0/archive/ s3://jmahe --server-side-encryption
 
 who -r |
 	read x x x date_boot hm
@@ -14,8 +16,8 @@ echo $hm |
 	IFS=: read heure_boot x
 
 if [[ $date_boot = $date_backup && $heure_boot = 04 && $heure_backup = 04 ]] ;then
-	echo "Arret systeme" >&2
+	echo "Arret systeme"
 	shutdown
 else
-	echo "Lancement manuel: pas d'arret systeme" >&2
+	echo "Lancement manuel: pas d'arret systeme"
 fi
